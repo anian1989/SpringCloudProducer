72445e2d8a4ca67bd0bcc00413ecc7ff23fd25aa
junshuaizhang1 <junshuaizhang@163.com>
2018-03-20 15:28:48 +0800

    Initial commit

---
+++ src/main/java/com/qinshihuang/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.qinshihuang.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by junshuaizhang1 on 2018/3/19.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is first messge";
+    }
+}
